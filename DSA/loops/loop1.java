c34aa1432eda6c0c29069607d60846408d0865c3
Gaurav <royg48848@gmail.com>
2025-03-27 18:45:58 +0530

    loop update

--- DSA/loops/loop1.java
+++ DSA/loops/loop1.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class loop1 {
     public static void main(String[] args) {
-        
+//        fruit switch case
+        System.out.println("Enter the fruit (apple or banana) : ");
+        Scanner sc = new Scanner(System.in);
+        String fruit = sc.next();
+        switch (fruit) {
+            case "apple":
+                System.out.println("doctors away");
+                break;
+            case "banana":
+                System.out.println("banana");
+                break;
+            default:
+                System.out.println("nothing");
+                break;
+        }
     }
 }
